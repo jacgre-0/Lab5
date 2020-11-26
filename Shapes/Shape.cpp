ac0f3edc0655ff9ec1de05b084e22f205b251ef0
_Jacce_ <jacob.ogren2@gmail.com>
2020-11-26 08:39:30 +0100

    Added setters for the color components

--- Shapes/Shape.cpp
+++ Shapes/Shape.cpp
@@ -1,4 +1,5 @@
 #include "Shape.h"
+#include <iostream>
 
 Shape::Shape(uint8_t r, uint8_t g, uint8_t b, uint8_t a) {
     color = a << 24 | r << 16 | g << 8 | b;
@@ -18,4 +19,36 @@ uint8_t Shape::GetGreen() const {
 
 uint8_t Shape::GetBlue() const {
     return color & 0xFF;
+}
+
+void Shape::SetAlpha(uint8_t value) {
+    if (value < 0 || value > 255) {
+        std::cout << "Value must ne between 0 and 255!" << std::endl;
+        return;
+    }
+    color = (color & 0x00FFFFFF) | (value << 24);
+}
+
+void Shape::SetRed(uint8_t value) {
+    if (value < 0 || value > 255) {
+        std::cout << "Value must ne between 0 and 255!" << std::endl;
+        return;
+    }
+    color = (color & 0xFF00FFFF) | (value << 16);
+}
+
+void Shape::SetGreen(uint8_t value) {
+    if (value < 0 || value > 255) {
+        std::cout << "Value must ne between 0 and 255!" << std::endl;
+        return;
+    }
+    color = (color & 0xFFFF00FF) | (value << 8);
+}
+
+void Shape::SetBlue(uint8_t value) {
+    if (value < 0 || value > 255) {
+        std::cout << "Value must ne between 0 and 255!" << std::endl;
+        return;
+    }
+    color = (color & 0xFFFFFF00) | value;
 }
--- Shapes/Shape.h
+++ Shapes/Shape.h
@@ -9,6 +9,10 @@ public:
     uint8_t GetRed() const;
     uint8_t GetGreen() const;
     uint8_t GetBlue() const;
+    void SetAlpha(uint8_t value);
+    void SetRed(uint8_t value);
+    void SetGreen(uint8_t value);
+    void SetBlue(uint8_t value);
     virtual void Render() {};
 private:
     int color;
